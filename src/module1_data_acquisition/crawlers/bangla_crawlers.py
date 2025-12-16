f0f05bbbccd89b0f1a11fa96a68a941471f22166
Siyam-Bhuiyan <111516393+Siyam-Bhuiyan@users.noreply.github.com>
2025-12-16 17:17:00 +0600

    Enhanced Bangla crawlers with more sources and AJAX support
    Expanded the list of start URLs and improved selectors for Prothom Alo and Bangla Tribune crawlers. Added support for AJAX 'load more' buttons with appropriate selectors, click limits, and delays to enable deeper crawling of dynamically loaded content.

--- src/module1_data_acquisition/crawlers/bangla_crawlers.py
+++ src/module1_data_acquisition/crawlers/bangla_crawlers.py
@@ -4,28 +4,34 @@ from .selenium_crawler import SeleniumCrawler
 def get_bangla_crawlers():
     crawlers = []
     
-    # 1. Prothom Alo (Selenium required for dynamic categories)
+    # 1. Prothom Alo (Selenium with AJAX load more button)
     crawlers.append(SeleniumCrawler(
         base_url="https://www.prothomalo.com/",
         start_urls=[
-            "https://www.prothomalo.com/",
             "https://www.prothomalo.com/collection/latest",
             "https://www.prothomalo.com/bangladesh",
+            "https://www.prothomalo.com/bangladesh/district",
+            "https://www.prothomalo.com/bangladesh/capital",
             "https://www.prothomalo.com/politics",
             "https://www.prothomalo.com/world",
             "https://www.prothomalo.com/sports",
-            "https://www.prothomalo.com/business"
+            "https://www.prothomalo.com/business",
+            "https://www.prothomalo.com/entertainment",
+            "https://www.prothomalo.com/opinion",
+            "https://www.prothomalo.com/lifestyle"
         ],
         language="bangla",
         source_name="prothom_alo",
         selectors={
-            'article_links': 'a', 
-            'title': 'h1',
-            'body': 'div.story-element-text, div.story-content, article',
+            'article_links': 'a[href*="/bangladesh/"], a[href*="/politics/"], a[href*="/world/"], a[href*="/sports/"], a[href*="/business/"], a[href*="/entertainment/"], a[href*="/opinion/"], a[href*="/lifestyle/"]',
+            'title': 'h1, h3.headline-title',
+            'body': 'div.story-element-text, div.story-content p, article p',
             'date': 'time',
             'date_attr': 'datetime'
         },
-        pagination_param='page'
+        load_more_selector='span.load-more-content, div.more, div._7ZpjE',
+        max_load_more_clicks=150,
+        delay=2.5
     ))
     
     # 2. BD News 24 (Selenium)
@@ -56,7 +62,7 @@ def get_bangla_crawlers():
         pagination_param='page'
     ))
 
-    # 4. Bangla Tribune
+    # 4. Bangla Tribune (with AJAX load more button)
     crawlers.append(SeleniumCrawler(
         base_url="https://www.banglatribune.com/",
         start_urls=[
@@ -65,18 +71,26 @@ def get_bangla_crawlers():
             "https://www.banglatribune.com/politics",
             "https://www.banglatribune.com/tech-and-gadget",
             "https://www.banglatribune.com/country",
-            "https://www.banglatribune.com/business"
+            "https://www.banglatribune.com/business",
+            "https://www.banglatribune.com/sports",
+            "https://www.banglatribune.com/entertainment",
+            "https://www.banglatribune.com/opinion",
+            "https://www.banglatribune.com/international",
+            "https://www.banglatribune.com/life-style",
+            "https://www.banglatribune.com/literature",
         ],
         language="bangla",
         source_name="bangla_tribune",
         selectors={
-            'article_links': 'a',  
+            'article_links': 'a.link_overlay',  
             'title': 'h1.title',   
-            'body': 'div.viewport.jw_article_body', 
-            'date': 'span.time_rel',
-            'date_attr': 'title'
+            'body': 'div.jw_article_body', 
+            'date': 'span.time.aitm',
+            'date_attr': 'data-published'
         },
-        pagination_param='page'
+        load_more_selector='button[id^="ajax_load_more_"]',
+        max_load_more_clicks=100,
+        delay=1.5
     ))
     
     # 5. Dhaka Post
